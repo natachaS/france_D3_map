a11ce2c8524b502977b01667d287bd69484dc018
natachas <natacha.springer@gmail.com>
2014-07-19 14:13:10 -0400

    fully qualified url for departements.json

--- script.js
+++ script.js
@@ -31,7 +31,7 @@
   
     // load all GeoJSON data
    
-  d3.json('departements.json', function(req, geojson) {
+  d3.json('http://natachaS.github.io/france_D3_map/departements.json', function(req, geojson) {
  
     // We bind a svg element for each GeoJSON entry
 
